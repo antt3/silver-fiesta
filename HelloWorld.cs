b013d0fd42036b63e386e3dd5c0587c13811ca2b
antt3 <anthonyt3@sbcglobal.net>
2024-06-30 11:02:39 -0700

    Initial Project Code

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace HelloWorld {
+		static public class Speaking {
+				public static string SahHello() {
+						return "Hello World";
+				}
+		}
+}
