50c82b1c7db63bf8de5e312db7ce178fd40240a0
antt3 <anthonyt3@sbcglobal.net>
2024-06-30 11:11:04 -0700

    gitignore .vs File

--- LeWebApp/LeWebApp/obj/Debug/net8.0/LeWebApp.AssemblyInfo.cs
+++ LeWebApp/LeWebApp/obj/Debug/net8.0/LeWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b013d0fd42036b63e386e3dd5c0587c13811ca2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
